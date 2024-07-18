e567c5736ae63cc2614c6d6e49c9d4ee07aee8f8
G384742023 <238474@st.takushoku-u.ac.jp>
2024-07-18 16:06:24 +0900

    プロジェクト

---
+++ weather.js
@@ -0,0 +1,44 @@
+
+print(data);
+function print(data){
+  let b = document.querySelector('#id');
+b.addEventListener('click', id);
+
+
+// 通信を開始する処理
+function id() {
+    
+    let url = 'https://www.nishita-lab.org/web-contents/jsons/openweather/{id}.json';
+
+    // 通信開始
+    axios.get(url)
+        .then(showResult)   // 通信成功
+        .catch(showError)   // 通信失敗
+        .then(finish);      // 通信の最後の処理
+}
+
+// 通信が成功した時の処理
+function showResult(resp) {
+    // サーバから送られてきたデータを出力
+    let data = resp.data;
+
+    // data が文字列型なら，オブジェクトに変換する
+    if (typeof data === 'string') {
+        data = JSON.parse(data);
+    }
+    print(data);
+}
+
+// 通信エラーが発生した時の処理
+function showError(err) {
+    console.log(err);
+}
+
+// 通信の最後にいつも実行する処理
+function finish() {
+    console.log('Ajax 通信が終わりました');
+}
+}
+
+
+
